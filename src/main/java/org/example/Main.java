d200c11cd3b69e421008151f55de946247ad63fe
Loïc <contact@narigane3.fr>
2023-06-13 10:44:53 +0200

    Create connection to MangoDB and set .env for secure datas

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,30 @@
 package org.example;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import io.github.cdimascio.dotenv.Dotenv;
+
+
 public class Main {
+    private static final Dotenv dotenv = Dotenv.load();
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+    // login url for mongodb atlas cluster get from env variable
+    private static final String connectionString = "mongodb+srv://" + dotenv.get("DB_PASSWORD") + ":" + dotenv.get("DB_USERNAME") + "@devdescktop.vpi0ffm.mongodb.net/";
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        logger.info("Welcome to my Body");
+
+        logger.info(connectionString);
+
+        try (MongoClient mongoClient = MongoClients.create(connectionString)) {
+
+            logger.info("Database connection successful");
+        } catch (Exception e) {
+            logger.error("An error occurred during connection ==> {}", e);
+        }
+
     }
 }
